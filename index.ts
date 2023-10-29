fda3a61f4affd72e9208efb9b1bccfbc10d46776
LAIBANADEEM106 <139864626+LaibaNadeem106@users.noreply.github.com>
2023-10-29 22:53:14 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,68 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const studentList : any = [];
+
+async function addStudent() {
+    const answerType = await inquirer.prompt([
+        {
+            type: "input",
+            name: "fullname",
+            message: "Enter the student's full name:",
+        },
+        {
+            type: "number",
+            name: "id",
+            message: "Enter the student's ID:",
+        },
+        {
+            type: "input",
+            name: "CoursesEnrolled",
+            message: "Enter the courses the student is enrolled in:",
+        },
+        {
+            type: "number",
+            name: "balance",
+            message: "Enter the student's balance:",
+        },
+    ]);
+
+    let { fullname, id, CoursesEnrolled, balance } = answerType;
+    const student: {
+        fullname: string;
+        id: number;
+        CoursesEnrolled: string;
+        balance: number;
+    } = { fullname, id, CoursesEnrolled, balance };
+    studentList.push(student);
+
+    console.log(chalk.green("Student added successfully!"));
+
+    const addMore = await inquirer.prompt([
+        {
+            type: "confirm",
+            name: "addMore",
+            message: "Do you want to add another student?",
+        },
+    ]);
+
+    if (addMore.addMore) {
+        addStudent();
+    } else {
+        displayStudentList();
+    }
+}
+
+function displayStudentList() {
+    console.log(chalk.blue("List of Students:"));
+    studentList.forEach((student:any, index: number) => {
+        console.log(chalk.bold(`Student ${index + 1}:`));
+        console.log(`Full Name: ${student.fullname}`);
+        console.log(`ID: ${student.id}`);
+        console.log(`Courses Enrolled: ${student.CoursesEnrolled}`);
+        console.log(`Balance: ${student.balance}`);
+        console.log();
+    });
+}
+
+addStudent();
